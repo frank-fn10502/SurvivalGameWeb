ac9e6a4579dc8b9b6c94bb3b8ed9ddf9ddff8ce0
Frank_Chen <frank08.chen@gmail.com>
2020-05-23 17:16:28 +0800

    view with new Web template "shop.cshtml" finished

--- SurvivalGame/Assets/javaScript/productMenu/ExteriorHelper.js
+++ SurvivalGame/Assets/javaScript/productMenu/ExteriorHelper.js
@@ -1,10 +1,8 @@
-function createEntity(objData ,templateList ,title = '',numberNo = -99)
-{
-    if(Array.isArray(objData))
-    {
+function createEntity(objData, templateList, title = '', numberNo = -99) {
+    if (Array.isArray(objData)) {
         let div = document.createElement('div');
-        objData.forEach((x,y) =>{
-            div.appendChild(createEntity(x ,templateList ,y));
+        objData.forEach((x, y) => {
+            div.appendChild(createEntity(x, templateList, y));
         });
         return div;
     }
@@ -12,44 +10,53 @@ function createEntity(objData ,templateList ,title = '',numberNo = -99)
 
     let template = document.querySelector(templateID).content.cloneNode(true);
     let wrap = document.createElement('div');
-    wrap.appendChild(template);   
-    
-    var re = /{{\w+}}/gi;
-    
+    wrap.appendChild(template);
+
+    var re = /{{\w+:*\w*}}/gi;
+
     wrap.innerHTML = wrap.innerHTML.replace(re, function (match) {
-        let key = match.substring(2).slice(0, -2).toLowerCase();
-        let replaceKey = Object.keys(objData).find(x => x.toLowerCase() == key);
+        let key = match.substring(2).slice(0, -2).toLowerCase().split(':');
+        let replaceKey = Object.keys(objData).find(x => x.toLowerCase() == key[0]);
 
         if (replaceKey) {
             let data = objData[replaceKey];
-            if (typeof data != 'object') {
+            // console.log('data', data, templateList);
+            if (key.length > 1) {
+                let pattern = templateList.find(x => {
+                    if (Array.isArray(x)) {
+                        console.log(match ,x ,x.some(y => y.substring(1).toLowerCase() == key[1].toLowerCase()));
+                        return x.some(y => y.substring(1).toLowerCase() == key[1].toLowerCase());
+                    }
+                    else return x.substring(1).toLowerCase() == key[1].toLowerCase();
+                });
+                let result = Array.isArray(pattern) ? pattern : [`${pattern}`];
+
+                let testCreate = createEntity(data, result);
+                
+                return testCreate.innerHTML.toString();
+            }
+            else if (typeof data != 'object') {
                 let imgReg = /(https:\/\/|.+)(\/.+)*(.jpg|.png|.+random)/ //
-                if(data.toString().match(imgReg))
-                {
+                if (data.toString().match(imgReg)) {
                     return `<img src="${data}" alt="imgGenByFunc">`;
                 }
-                else
-                {
+                else {
                     return data;
                 }
             }
-            else if(Array.isArray(data)){
+            else if (Array.isArray(data)) {
                 let str = '';
-                data.forEach((x ,y) =>{
-                    str += createEntity(x ,templateList.slice(1) ,Object.values(x)[0] ,y).outerHTML.toString();
+                data.forEach((x, y) => {
+                    str += createEntity(x, templateList.slice(1), Object.values(x)[y], y).outerHTML.toString();
                 });
                 return str;
             }
-            //未完成 obj > obj
-            // else
-            // {
-            //     return createEntity(objData ,templateList.splice(0,1));
-            // }
         }
-        else
-        {
+        else {
             return `{{${key}}}`;
         }
-    }).replace(re ,`${title}-${numberNo}`);
+    }).replace(re, `${title}-${numberNo}`);
+
+    
     return wrap.children[0];
 }
